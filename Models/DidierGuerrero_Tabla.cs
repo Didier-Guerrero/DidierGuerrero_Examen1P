d87ceb510339713dbe0ae1411763fad03e9c9e89
Didier Guerrero <didier.guerrero@udla.edu.ec>
2023-10-25 08:27:45 -0500

    Creacion de tabla Entradas

---
+++ Models/DidierGuerrero_Tabla.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DidierGuerrero_Examen1P.Models
+{
+    public class DidierGuerrero_Tabla
+    {
+        [Required]
+        public int dgEntradasDisponibles { get; set; }
+        [Range(4.50, 8.50)]
+        public decimal dgPrecio { get; set; }
+        [MinLength(5)]
+        [Display(Name = "Nombre de pelicula")]
+        public string? dgNombrePelicula { get; set; }
+        [Required]
+        public DateTime? dgFechaPelicula { get; set; }
+        public bool MdgMayorEdad { get; set; }
+    }
+}
