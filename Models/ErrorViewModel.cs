d893d18c3990277b195de2c0fc12ae49a7ea7be1
Didier Guerrero <didier.guerrero@udla.edu.ec>
2023-10-25 08:27:02 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DidierGuerrero_Examen1P.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
